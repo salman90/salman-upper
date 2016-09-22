76f6e351bc1745bb3675a5520e821a556b3f8426
Kenan Philips <philipskenan@gmail.com>
2016-09-22 00:00:26 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+function respond() {
+  // Put all responsive functions here
+}
+
+$(document).ready(function() {
+});
+
+$(window).load(function() {
+});
+
+$(window).resize(function() {
+  respond();
+});
+
+$(window).scroll(function() {
+});
