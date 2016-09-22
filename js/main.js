634392fbbcd71ca119e6ef766ffc2c4cd16f8c43
Kenan Philips <philipskenan@gmail.com>
2016-09-22 00:17:59 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+function backgroundVideo() {
+  var winHeight = $(window).height();
+  $('video').css({  'height': winHeight, 'width': '100%'});
+}
 
 function respond() {
   // Put all responsive functions here
+  // backgroundVideo();
 }
 
 $(document).ready(function() {
